0dd4dcd109d0796cfcb8c75d885198d3edb195d2
Syed Ali <syedsadiqali2018@gmail.com>
2024-03-25 19:19:44 +0530

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import { Toaster } from "@/components/ui/toaster";
 import NextTopLoader from "nextjs-toploader";
 import { ThemeProvider } from "@/components/theme-provider";
-import "./globals.css";
 import TopLoader from "./top-loader";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import "./globals.css";
 
 export const metadata = {
   title: "LinkTap",
@@ -14,6 +15,7 @@ export default async function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <body className="flex flex-col">
+        <SpeedInsights/>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
